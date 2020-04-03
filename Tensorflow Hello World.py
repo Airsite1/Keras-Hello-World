8dc388b54904fb652eec494e6300b53c7c688891
Sanjay S <srikumar.sanjay@gmail.com>
2020-04-03 14:23:11 -0400

    Create Tensorflow Hello World.py

---
+++ Tensorflow Hello World.py
@@ -0,0 +1,11 @@
+from tensorflow import keras
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+import numpy as np
+
+xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0 ],dtype=float)
+ys = np.array([1.0, 1.5, 2.0, 2.5, 3.0, 3.5 ],dtype=float)
+model = keras.Sequential([keras.layers.Dense(units=1,input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+model.fit(xs,ys, epochs=600)
+print(model.predict([7.0]))
